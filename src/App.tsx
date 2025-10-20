3ab8700f915081b64b970894701ece4105147caf
misadeks <mihajlo.krunic@hotmail.com>
2025-10-20 22:53:30 +0200

    added questions fetching and query for them

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import './App.css';
 
-import { useCategoriesQuery } from './data/api/openTrivia';
+import {useCategoriesQuery, useQuestionsQuery} from './data/api/openTrivia';
 
 
 function App() {
   const {data: categories} = useCategoriesQuery();
+  const {data: questions} = useQuestionsQuery();
 
   return (
-    <div className="App">
-      {categories?.map(value => <p>{value.name}</p>)}
-    </div>
+      <>
+        <div className="App">
+          {categories?.map(value => <p>{value.name}</p>)}
+        </div>
+        <div className="ques">
+          {questions?.map(value => <p>{value.question}</p>)}
+        </div>
+      </>
   );
 }
 
--- src/data/api/openTrivia.ts
+++ src/data/api/openTrivia.ts
@@ -1,6 +1,7 @@
 import { useQuery, UseQueryResult } from '@tanstack/react-query';
 
 const API_CATEGORIES_URL= 'https://opentdb.com/api_category.php';
+const API_QUESTIONS_URL = 'https://opentdb.com/api.php?amount=50';
 
 export type TriviaQuestion = {
     category: string
@@ -23,10 +24,32 @@ async function fetchCategories(): Promise<TriviaCategory[]> {
         }).then((data) => data.trivia_categories || []);
 }
 
+async function fetchQuestions():Promise<TriviaQuestion[]> {
+    return fetch(API_QUESTIONS_URL).then((res) => {
+        if (!res.ok) {
+            throw new Error('Failed to load questions!');
+        }
+        return res.json();
+    }).then((data) => {
+        if (data.response_code !== 0) {
+            throw new Error(`OpenTDB error ${data.response_code}`);
+        }
+        return data.results;
+    })
+}
+
 export function useCategoriesQuery(): UseQueryResult<TriviaCategory[], Error>{
     return useQuery({
         queryKey: ['categories'],
         queryFn: fetchCategories,
+        staleTime: 60 * 60 * 1000, // 1h
+    });
+}
+
+export function useQuestionsQuery(): UseQueryResult<TriviaQuestion[], Error>{
+    return useQuery({
+        queryKey: ['questions'],
+        queryFn: fetchQuestions,
         staleTime: 60 * 60 * 1000, // 1h
     });
 }
